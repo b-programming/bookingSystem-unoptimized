4f2bbb423a02d3611006ff8e3e72684ce0d78c31
b-programming <martin_breznik@hotmail.com>
2020-02-17 19:41:10 +0000

    fix

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import {Redirect } from "react-router-dom";
+import React from 'react';
+import { Redirect } from "react-router-dom";
 const axios = require('axios');
 const moment = require('moment');
 class Form extends React.Component {
 constructor(props){
   super(props);
   this.state = {
-      success: false,
       workHours: [],
       appointments: [],
       inputDate: {},
@@ -18,6 +17,8 @@ constructor(props){
       fullDay:false,
       noBarberD:[],
       freeHours:[ {id:1}, {value: "choose date"}],
+      bookedHours:[],
+      openClose:[],
       selectedHour:'',
       firstWeek: true
   };
@@ -28,7 +29,6 @@ constructor(props){
   this.calcAppointment = this.calcAppointment.bind(this);
 }
 async componentWillMount(){
-    this.setState({loading: true});
     const wHours = await axios.get('http://127.0.0.1:5000/workHours')
     const workHours = await wHours.data
     this.setState({ workHours });
@@ -44,23 +44,21 @@ async componentWillMount(){
     const barberss = await axios.get('http://127.0.0.1:5000/barbers')
     const barbers = await barberss.data
     this.setState({ barbers });
-    this.setState({loading: false});
 }
 
 calcAppointment() {
     const bookedArr = [];
     const noBarberDays = [];
+    const openClose = [];
     this.state.barbers.forEach((barbers) => {
     var i;
-    for(i=0;i<11;i++){
+    for(i=0;i<10;i++){
     if(i<=5){
         var openDate = moment(`1970-01-01 ${this.state.workHours[0].startHour}:00`).format('HH:mm');
         var closeDate = moment(`1970-01-01 ${this.state.workHours[0].endHour}:00`).format('HH:mm');
         if(i<=3){
             var shiftStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].startHour}:00`).format('HH:mm');
             var shiftEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].endHour}:00`).format('HH:mm');
-            var earlyBreakStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].lunchTime.startHour}:00`).format('HH:mm');
-            var earlyBreakEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].lunchTime.startHour}:30`).format('HH:mm');
             }
         if(i>3){
             noBarberDays.push(i);
@@ -75,20 +73,22 @@ calcAppointment() {
         if(i>8){
             var shiftStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].startHour}:00`).format('HH:mm');
             var shiftEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].endHour}:00`).format('HH:mm');
-            var earlyBreakStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].lunchTime.startHour}:00`).format('HH:mm');
-            var earlyBreakEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].lunchTime.startHour}:30`).format('HH:mm');
         }
 }
 if(openDate < shiftStart){openDate=shiftStart;}
 if(closeDate > shiftEnd){closeDate=shiftEnd;}
+openClose.push({from: openDate, to: closeDate });
 }
+this.setState({ openClose: openClose }, function () {
+  console.log("from to:", this.state.openClose);
+}); 
+
 this.state.appointments.forEach((appointment) => {
-  //var startDate = moment.unix(appointment.startDate).format('HH:mm');
   bookedArr.push({from: appointment.startDate, service: appointment.serviceId });
 });
     });
     //booked from to dates
-    console.log(bookedArr);
+
      this.setState({ booked: bookedArr }, function () {
         console.log("booked arr:", this.state.booked);
     }); 
@@ -104,12 +104,12 @@ console.log( selectedDay, serviceTime, alreadyBooked);
 if(this.state.firstWeek == true){
   var earlyBreakStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].lunchTime.startHour}:00`).format('HH:mm');
   var earlyBreakEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[0].lunchTime.startHour}:30`).format('HH:mm');
-  bookedHours.push({from: earlyBreakStart, to: earlyBreakEnd });
+  bookedHours.push({from: earlyBreakStart, to: earlyBreakEnd});
 }
 if(this.state.firstWeek ==false){
   var earlyBreakStart = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].lunchTime.startHour}:00`).format('HH:mm');
   var earlyBreakEnd = moment(`1970-01-01 ${this.state.barbers[0].workHours[4].lunchTime.startHour}:30`).format('HH:mm');
-  bookedHours.push({from: earlyBreakStart, to: earlyBreakEnd });
+  bookedHours.push({from: earlyBreakStart, to: earlyBreakEnd});
 }
  var arr = [];
 alreadyBooked.forEach((booked) => {
@@ -117,7 +117,6 @@ alreadyBooked.forEach((booked) => {
     var appointmentDay = moment.unix(booked.from).format('DD');
     var appointmentHour = moment.unix(booked.from).format('HH:mm');
     arr.push({month: appointmentMonth, day: appointmentDay });
-    console.log(arr);
     if(selectedMonth == appointmentMonth){
     if(selectedDay == appointmentDay){
       if(serviceTime.durationInMin == 20){
@@ -135,16 +134,19 @@ alreadyBooked.forEach((booked) => {
     }
   }
   if(selectedMonth !== appointmentMonth){
-    console.log("free month");
+    //console.log("free month");
     if(selectedDay !== appointmentDay){
-      console.log("free day");
+      //console.log("free day");
     }
   }
 })
-console.log(bookedHours);
+this.setState({ bookedHours: bookedHours }, function () {
+  console.log(this.state.bookedHours);
+}); 
 }
 
   onChangeDate(e) {
+    e.preventDefault();
     var firstParse = moment(e.target.value).format('DD.MM.YYYY');
     this.setState({ inputDate: firstParse }, function () {
         console.log(this.state.inputDate);
@@ -160,30 +162,89 @@ console.log(bookedHours);
     }
     return businessDays;
   }
-  var diffInDays = calcBusinessDays(barbersFirstDay, clientSelectedDate);
-  var firstWeek = diffInDays * 5;
+  var diffInDays = calcBusinessDays(barbersFirstDay, clientSelectedDate) - 1;
+  var firstWeek = Math.floor(diffInDays / 5);
   firstWeek = firstWeek % 2;
   var Worked14D = Math.floor(diffInDays / 10); //10 days considerered 1 loop
   var daysWorkedThisWeek = diffInDays % 10;
+  var daysWorkedThisFiveDayWeek = diffInDays % 5 - 1;
   var barberAbsent = this.state.noBarberD.includes(daysWorkedThisWeek);
+  var freeHoursOnDay = [];
   this.setState({fullDay: barberAbsent});
+  if(daysWorkedThisFiveDayWeek < 0){daysWorkedThisFiveDayWeek = 4;}
   if(firstWeek == 0){
     this.setState({ firstWeek: false }, function () {
       console.log(this.state.firstWeek);
      });
+     var start = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek + 5].from}`);
+     var finish = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek + 5].to}`);
+     var durations = this.state.inputService[0].durationInMin;
+     //var newDate = start.add(durations, "minutes"); moment1.isSameOrAfter(moment2);
+     freeHoursOnDay.push(start.format('HH:mm'));
+     /* for(var i=0;(i<=freeHoursOnDay.length)&&(freeHoursOnDay[i].start.isSameOrAfter(start)&&(freeHoursOnDay[i].start.isSameOrBefore(finish)));i++){ */
+      var freeTime = finish.diff(start, 'm');
+      var maxBooked = Math.floor(freeTime / durations);
+      var startOfAppointment = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek + 5].from}`);
+      for (var i = 1; i < maxBooked; i++) {
+        // startOfAppointment!==this.state.bookedHours[i].from
+        var firstBreak = moment(`1970-01-01 ${this.state.bookedHours[0].from}`);
+        var firstBreakEnd = moment(`1970-01-01 ${this.state.bookedHours[0].to}`);
+        if(start <= startOfAppointment && startOfAppointment<finish){
+        if(startOfAppointment<firstBreak){
+          freeHoursOnDay.splice(i,0, startOfAppointment.add(durations, 'minutes').format('HH:mm'))
+          }
+        if(startOfAppointment>=firstBreak){
+          freeHoursOnDay.splice(i,0, startOfAppointment.add(durations, 'minutes').format('HH:mm'))
+          }
+        }        
+      }
+      
   }
   if(firstWeek == 1){
     this.setState({ firstWeek: true }, function () {
       console.log(this.state.firstWeek);
      });
+     var start = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek].from}`);
+     var finish = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek].to}`);
+     var durations = this.state.inputService[0].durationInMin;
+     freeHoursOnDay.push(start.format('HH:mm'));
+      var freeTime = finish.diff(start, 'm');
+      var maxBooked = Math.floor(freeTime / durations);
+      var startOfAppointment = moment(`1970-01-01 ${this.state.openClose[daysWorkedThisFiveDayWeek + 5].from}`);
+      console.log("max", maxBooked);
+      
+      for (var i = 1; i < maxBooked; i++) {
+        var firstBreak = moment(`1970-01-01 ${this.state.bookedHours[0].from}`);
+        var firstBreakEnd = moment(`1970-01-01 ${this.state.bookedHours[0].to}`);
+        if(start <= startOfAppointment && startOfAppointment<finish){
+          if(startOfAppointment<firstBreak){
+        freeHoursOnDay.splice(i,0, startOfAppointment.add(durations, 'minutes').format('HH:mm'))
+        }
+          if(startOfAppointment>=firstBreakEnd){
+            freeHoursOnDay.splice(i,0, startOfAppointment.add(durations, 'minutes').format('HH:mm'))
+          }
+      }
+      }
   }
   if(barberAbsent == true){
     this.setState({ freeHours: [{id: 0, value: 'no apointments available'}] }, function () {
         console.log(this.state.selectedHour);
        });
     }
+    if(barberAbsent == false){
+      var arr = [];
+      for(var i=0;i<freeHoursOnDay.length;i++){
+      arr.push({id:i, value: freeHoursOnDay[i]});
+      }
+      console.log(arr);
+       this.setState({ freeHours: arr }, function () {
+        console.log(this.state.freeHours);
+       });
+    }
+    console.log(this.state.bookedHours, this.state.openClose);
 }
   onChangeService(e) {
+    e.preventDefault();
     if(e.target.value == 1){
       this.setState({ inputService: [{id: e.target.value, durationInMin: 20}] });
     }
@@ -193,7 +254,6 @@ console.log(bookedHours);
     if(e.target.value == 3){
       this.setState({ inputService: [{id: e.target.value, durationInMin: 50}] });
     }
-
   }
   onChangeTime(e){
     this.setState({ selectedHour: e.target.value }, function () {
